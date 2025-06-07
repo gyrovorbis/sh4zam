be067846a2642252631aa30d966fa6814692d4d5
Falco Girgis <gyrovorbis@gmail.com>
2025-06-07 09:33:20 -0500

    Optimized shz_memcpy32() even further.
    Was able to get even further gainz by reworking the logic to fit into
    cache lines better and allow me to make the function inline.

--- include/sh4zam/shz_mem.h
+++ include/sh4zam/shz_mem.h
@@ -89,7 +89,39 @@ SHZ_INLINE void *shz_memcpy4(void *SHZ_RESTRICT dst, const void *SHZ_RESTRICT sr
 SHZ_CONST void *shz_memcpy8(void *SHZ_RESTRICT dst, const void *SHZ_RESTRICT src, size_t bytes);
 SHZ_CONST void *shz_memset8(void *dst, uint64_t value, size_t bytes);
 
-SHZ_CONST void *shz_memcpy32(void *SHZ_RESTRICT dst, const void *SHZ_RESTRICT src, size_t bytes);
+SHZ_INLINE void *shz_memcpy32(void *SHZ_RESTRICT dst, const void *SHZ_RESTRICT src, size_t bytes) {
+    size_t cnt = (bytes >> 5);
+    
+    if(cnt) {
+        asm volatile(R"( 
+            pref      @%[src]           ! Prefetch src start 
+            fschg                       ! Swap to double FMOV mode
+        1:
+            movca.l   r0, @%[dst]       ! Preallocate cache line (overwriting existing)  
+            dt        %[cnt]            ! Decrement + test if r6 is zero       
+            add       #32, %[dst]       ! Pre-increment dst pointer by 4 doubles            
+            fmov.d    @%[src]+, dr4
+            fmov.d    @%[src]+, dr6   
+            fmov.d    @%[src]+, dr8     ! Load 4 8-byte doubles into FP regs,
+            fmov.d    @%[src]+, dr10    !   incrementing src by 8 bytes each     
+            fmov.d    dr10, @-%[dst]  
+            fmov.d    dr8,  @-%[dst]     
+            fmov.d    dr6,  @-%[dst]    ! Store 4 8-byte doubles from FP regs,          
+            fmov.d    dr4,  @-%[dst]    !   decrementing dst by 8 bytes each 
+            bt/s      2f
+            add       #32, %[dst]       ! Increment dst to next block   
+            bra       1b                ! Continue looping until we only have 1 iteration
+            pref      @%[src]           ! Prefetch src start             
+        2:    
+            fschg                       ! Swap back to float FMOVs
+        )"
+        : [dst] "+r" (dst), [src] "+r" (src), [cnt] "+r" (cnt)
+        :
+        : "memory");
+    }
+
+    return dst;
+}
 
 SHZ_DECLS_END
 
