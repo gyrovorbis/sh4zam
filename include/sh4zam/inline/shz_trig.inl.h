89cac0f6b98e42cc7914c54a2908f0ec425062a8
Falco Girgis <gyrovorbis@gmail.com>
2026-02-27 15:10:32 -0600

    Cleanup + pristinification of TRIG API + arch.

--- include/sh4zam/inline/shz_trig.inl.h
+++ include/sh4zam/inline/shz_trig.inl.h
@@ -15,9 +15,9 @@
 
 #if SHZ_BACKEND == SHZ_SH4
 #   include "sh4/shz_trig_sh4.inl.h"
+#else
+#   include "sw/shz_trig_sw.inl.h"
 #endif
-
-#include "sw/shz_trig_sw.inl.h"
 
 SHZ_FORCE_INLINE shz_sincos_t shz_sincosu16(uint16_t radians16) SHZ_NOEXCEPT {
     if(__builtin_constant_p(radians16))
@@ -53,104 +53,132 @@ SHZ_FORCE_INLINE shz_sincos_t shz_sincosf_deg(float degrees) SHZ_NOEXCEPT {
 }
 
 SHZ_FORCE_INLINE float shz_sincos_tanf(shz_sincos_t sincos) SHZ_NOEXCEPT {
-    return shz_sincos_tanf_sw(sincos);
+    return shz_divf(sincos.sin, sincos.cos);
 }
 
 SHZ_FORCE_INLINE float shz_sincos_secf(shz_sincos_t sincos) SHZ_NOEXCEPT {
-    return shz_sincos_secf_sw(sincos);
+    return shz_invf(sincos.cos);
 }
 
 SHZ_FORCE_INLINE float shz_sincos_cscf(shz_sincos_t sincos) SHZ_NOEXCEPT {
-    return shz_sincos_cscf_sw(sincos);
+    return shz_invf(sincos.sin);
 }
 
 SHZ_FORCE_INLINE float shz_sincos_cotf(shz_sincos_t sincos) SHZ_NOEXCEPT {
-    return shz_sincos_cotf_sw(sincos);
+    return shz_divf(sincos.cos, sincos.sin);
 }
 
 SHZ_FORCE_INLINE float shz_sinf(float radians) SHZ_NOEXCEPT {
     if(__builtin_constant_p(radians))
         return __builtin_sinf(radians);
 
-    return shz_sinf_sw(radians);
+    return shz_sincosf(radians).sin;
 }
 
 SHZ_FORCE_INLINE float shz_sinf_deg(float degrees) SHZ_NOEXCEPT {
-    return shz_sinf_deg_sw(degrees);
+    return shz_sincosf_deg(degrees).sin;
 }
 
 SHZ_FORCE_INLINE float shz_cosf(float radians) SHZ_NOEXCEPT {
     if(__builtin_constant_p(radians))
         return __builtin_cosf(radians);
 
-    return shz_cosf_sw(radians);
+    return shz_sincosf(radians).cos;
 }
 
 SHZ_FORCE_INLINE float shz_cosf_deg(float degrees) SHZ_NOEXCEPT {
-    return shz_cosf_deg_sw(degrees);
+    return shz_sincosf_deg(degrees).cos;
 }
 
 SHZ_FORCE_INLINE float shz_tanf(float radians) SHZ_NOEXCEPT {
     if(__builtin_constant_p(radians))
         return __builtin_tanf(radians);
 
-    return shz_tanf_sw(radians);
+    return shz_sincos_tanf(shz_sincosf(radians));
 }
 
 SHZ_FORCE_INLINE float shz_tanf_deg(float degrees) SHZ_NOEXCEPT {
-    return shz_tanf_deg_sw(degrees);
+    return shz_sincos_tanf(shz_sincosf_deg(degrees));
 }
 
 SHZ_FORCE_INLINE float shz_secf(float radians) SHZ_NOEXCEPT {
     if(__builtin_constant_p(radians))
         return 1.0f / __builtin_cosf(radians);
 
-    return shz_secf_sw(radians);
+    return shz_sincos_secf(shz_sincosf(radians));
 }
 
 SHZ_FORCE_INLINE float shz_secf_deg(float degrees) SHZ_NOEXCEPT {
-    return shz_secf_deg_sw(degrees);
+    return shz_sincos_secf(shz_sincosf_deg(degrees));
 }
 
 SHZ_FORCE_INLINE float shz_cscf(float radians) SHZ_NOEXCEPT {
     if(__builtin_constant_p(radians))
         return 1.0f / __builtin_sinf(radians);
 
-    return shz_cscf_sw(radians);
+    return shz_sincos_cscf(shz_sincosf(radians));
 }
 
 SHZ_FORCE_INLINE float shz_cscf_deg(float degrees) SHZ_NOEXCEPT {
-    return shz_cscf_deg_sw(degrees);
+    return shz_sincos_cscf(shz_sincosf_deg(degrees));
 }
 
 SHZ_FORCE_INLINE float shz_cotf(float radians) SHZ_NOEXCEPT {
     if(__builtin_constant_p(radians))
         return 1.0f / __builtin_tanf(radians);
 
-    return shz_cotf_sw(radians);
+    return shz_sincos_cotf(shz_sincosf(radians));
 }
 
 SHZ_FORCE_INLINE float shz_cotf_deg(float degrees) SHZ_NOEXCEPT {
-    return shz_cotf_deg_sw(degrees);
+    return shz_sincos_cotf(shz_sincosf_deg(degrees));
 }
 
 SHZ_FORCE_INLINE float shz_atanf_unit(float x) SHZ_NOEXCEPT {
-    return shz_atanf_unit_sw(x);
+    const float n1 = 0.97239411f;
+    const float n2 = -0.19194795f;
+
+    return shz_fmaf(n2, x * x, n1) * x;
 }
 
 SHZ_INLINE float shz_atanf_q1(float x) SHZ_NOEXCEPT {
-    return shz_atanf_q1_sw(x);
+    return SHZ_F_PI_2 - shz_atanf_unit(shz_invf_fsrra(x));
 }
 
 SHZ_INLINE float shz_atanf(float x) SHZ_NOEXCEPT {
-    return shz_atanf_sw(x);
+    if(x > 1.0f)
+	    return shz_atanf_q1(x);
+    else if(x < -1.0f)
+        return -shz_atanf_q1(x);
+    else
+        return shz_atanf_unit(x);
 }
 
 SHZ_INLINE float shz_atan2f(float y, float x) SHZ_NOEXCEPT {
     if(__builtin_constant_p(y) && __builtin_constant_p(x))
         return __builtin_atan2f(y, x);
 
-    return shz_atan2f_sw(y, x);
+    float angle = SHZ_F_PI_2;
+    float r = x;
+    float abs_sum = shz_fabsf(y);
+
+    if(x <= 0.0f) {
+        if(SHZ_UNLIKELY(x == 0.0f && y == 0.0f))
+            return 0.0f;
+
+        angle += SHZ_F_PI_4;
+        r += abs_sum;
+        abs_sum -= x;
+    } else {
+        angle -= SHZ_F_PI_4;
+        r -= abs_sum;
+        abs_sum += x;
+    }
+
+    r *= shz_invf_fsrra(abs_sum);
+    angle += shz_fmaf(0.1963f, r * r, -0.9817f) * r;
+
+    return shz_copysignf(angle, y);
 }
 
 
@@ -158,35 +186,35 @@ SHZ_INLINE float shz_asinf(float x) SHZ_NOEXCEPT {
     if(__builtin_constant_p(x))
         return __builtin_asinf(x);
 
-    return shz_asinf_sw(x);
+return shz_atanf(x * shz_inv_sqrtf_fsrra(1.0f - (x * x)));
 }
 
 SHZ_INLINE float shz_acosf(float x) SHZ_NOEXCEPT {
     if(__builtin_constant_p(x))
         return __builtin_acosf(x);
 
-    return shz_acosf_sw(x);
+    return SHZ_F_PI_2 - shz_asinf(x);
 }
 
 SHZ_INLINE float shz_asecf(float x) SHZ_NOEXCEPT {
     if(__builtin_constant_p(x))
         return __builtin_acosf(1.0f / x);
 
-    return shz_asecf_sw(x);
+    return shz_acosf(shz_invf(x));
 }
 
 SHZ_INLINE float shz_acscf(float x) SHZ_NOEXCEPT {
     if(__builtin_constant_p(x))
         return __builtin_asinf(1.0f / x);
 
-    return shz_acscf_sw(x);
+    return shz_asinf(shz_invf(x));
 }
 
 SHZ_INLINE float shz_acotf(float x) SHZ_NOEXCEPT {
     if(__builtin_constant_p(x))
         return __builtin_atanf(1.0f / x);
 
-    return shz_acotf_sw(x);
+    return shz_atanf(shz_invf(x));
 }
 
 SHZ_FORCE_INLINE float shz_sinhf(float x) SHZ_NOEXCEPT {
